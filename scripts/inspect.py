29b5d0d079a62caf9295deaf285da862da78eb4c
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2025-08-05 05:51:35 -0500

    extract fn to load any manual module

--- scripts/inspect.py
+++ scripts/inspect.py
@@ -3,7 +3,7 @@ import importlib.util
 from pathlib import Path
 import random
 import sys
-
+from typing import Any
 from dataclasses_json import DataClassJsonMixin
 
 
@@ -19,31 +19,37 @@ class WorldData:
 
 
 def inspect_manual_world(world_dir: str | Path) -> WorldData:
+    data_module: Any = load_manual_world_module(world_dir, "Data")
+    return WorldData(
+        game_table=GameData.from_dict(data_module.game_table),
+    )
+
+
+def load_manual_world_module(world_dir: str | Path, module_name: str) -> object:
     src_dir = Path(world_dir) / "src"
+    module_file_path = src_dir / f"{module_name}.py"
 
     # create a random module name to avoid caching,
     # making sure we're always loading a fresh module
-    data_module_name = f"manual_data_{random.randbytes(16).hex()}"
+    module_key = f"manual_data_{random.randbytes(16).hex()}"
 
-    data_module_spec = importlib.util.spec_from_file_location(
-        name=data_module_name,
-        location=src_dir / "Data.py",
+    module_spec = importlib.util.spec_from_file_location(
+        name=module_key,
+        location=module_file_path,
         submodule_search_locations=[str(src_dir)],
     )
 
-    if not data_module_spec or not data_module_spec.loader:
-        raise Exception(f"Failed to create module spec for {src_dir / "Data.py"}")
+    if not module_spec or not module_spec.loader:
+        raise Exception(f"Failed to create module spec for {module_file_path}")
 
-    data_module = importlib.util.module_from_spec(data_module_spec)
-    sys.modules[data_module_name] = data_module
+    module = importlib.util.module_from_spec(module_spec)
+    sys.modules[module_key] = module
 
     original_sys_path = sys.path.copy()
     try:
         # sys.path.append(self.archipelago_repo_dir.as_posix())
-        data_module_spec.loader.exec_module(data_module)
+        module_spec.loader.exec_module(module)
     finally:
         sys.path = original_sys_path
 
-    return WorldData(
-        game_table=GameData.from_dict(data_module.game_table),
-    )
+    return module
