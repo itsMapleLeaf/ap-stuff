8a78c90191ec2f129efd42e7ace681a164b617b0
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2025-09-17 11:26:29 -0500

    fix export script

--- scripts/export.py
+++ scripts/export.py
@@ -4,46 +4,41 @@ import sys
 from showinfm.showinfm import show_in_file_manager
 import yaml
 
-from ._manual_worlds import list_project_manual_worlds
-from .build import make_apworld
-from ._paths import project_dir
+from .create_generate_players_dir import list_player_file_paths
+
+from ._paths import project_dir, user_archipelago_worlds_dir
 
 
 def __main():
     game_arg = sys.argv[1]
-
-    game_dir = project_dir / "games" / game_arg
     output_dir = project_dir / "dist/export" / game_arg
-
     combined_player_config_file_name = "Maple.yaml"
 
     shutil.rmtree(output_dir, ignore_errors=True)
     os.makedirs(output_dir)
 
-    for world in list_project_manual_worlds():
-        make_apworld(world.src_dir, output_dir=output_dir)
-
     player_configs = []
-    for player_config_file_name in os.listdir(game_dir):
-        if not player_config_file_name.endswith(".yaml"):
-            continue
-
-        with open(
-            game_dir / player_config_file_name, encoding="utf8"
-        ) as player_config_file:
+    for player_config_path in list_player_file_paths(game_arg):
+        with open(player_config_path, encoding="utf8") as player_config_file:
             player_configs.append(yaml.safe_load(player_config_file))
 
-    combined_player_config_path = output_dir / combined_player_config_file_name
+    combined_config_path = output_dir / combined_player_config_file_name
 
-    with open(
-        combined_player_config_path, "w", encoding="utf8"
-    ) as combined_player_config_file:
+    with open(combined_config_path, "w", encoding="utf8") as combined_config_file:
         yaml.dump_all(
             player_configs,
-            combined_player_config_file,
+            combined_config_file,
             sort_keys=False,
             allow_unicode=True,
         )
+
+    included_worlds = {
+        user_archipelago_worlds_dir / f"{config['game']}.apworld"
+        for config in player_configs
+    }
+
+    for world_path in included_worlds:
+        shutil.copy(world_path, output_dir)
 
     show_in_file_manager(str(output_dir))
 
