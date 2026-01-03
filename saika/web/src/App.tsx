83d25b56308ad13a233b3c28c3f8c4fec82df438
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2026-01-03 13:27:36 -0600

    saika: some more styling

--- saika/web/src/App.tsx
+++ saika/web/src/App.tsx
@@ -1,38 +1,45 @@
+import { Icon } from "@iconify/react"
+import { type JSX, useState } from "react"
+
 export function App() {
-	const viewMap = {
-		Connect: {
-			icon: "",
-			content: <p>Connect</p>,
-		},
-		// Connect: {
-		// 	icon: "",
-		// 	content: <p>Connect</p>,
-		// },
-		Settings: {
-			icon: "",
-			content: <p>Settings</p>,
-		},
+	type View = {
+		name: string
+		icon: string
+		content: JSX.Element
 	}
 
-	const views = Object.entries(viewMap).map(([name, config]) => ({
-		name,
-		...config,
-	}))
+	const views: [View, ...View[]] = [
+		{
+			name: "Connect",
+			icon: "mingcute:plugin-fill",
+			content: <p>Connect</p>,
+		},
+		{
+			name: "Settings",
+			icon: "mingcute:settings-2-fill",
+			content: <p>Settings</p>,
+		},
+	]
+
+	const [currentView, setCurrentView] = useState(views[0])
 
 	return (
-		<div className="flex h-dvh items-stretch gap-3 p-3">
-			<div className="flex w-48 flex-col gap-3">
+		<div className="flex h-dvh items-stretch gap-2 p-3">
+			<div className="flex w-48 flex-col gap-2">
 				{views.map((view) => (
 					<button
 						key={view.name}
 						type="button"
-						className="flex h-10 items-center justify-start rounded bg-gray-800 px-3"
+						data-current={view.name === currentView.name || undefined}
+						className="flex h-10 items-center justify-start gap-2 rounded px-3 opacity-75 transition duration-150 hover:bg-gray-800 hover:opacity-100 data-current:bg-gray-800 data-current:opacity-100"
+						onClick={() => setCurrentView(view)}
 					>
+						<Icon icon={view.icon} className="-mx-0.5 size-5" />
 						{view.name}
 					</button>
 				))}
 			</div>
-			<div className="flex-1 rounded bg-gray-800">view</div>
+			<div className="flex-1 rounded bg-gray-800">{currentView.content}</div>
 		</div>
 	)
 }
--- saika/web/src/main.tsx
+++ saika/web/src/main.tsx
@@ -1,6 +1,8 @@
+import "@fontsource-variable/rubik"
+import "./tailwind.css"
+
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
-import "./tailwind.css"
 import { App } from "./App.tsx"
 
 createRoot(document.getElementById("root") as HTMLElement).render(
