4824e1cea105a5e9e7a3beb770c23018f12f3a74
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2026-01-03 19:47:32 -0600

    closing sessions

--- saika/web/src/App.tsx
+++ saika/web/src/App.tsx
@@ -100,7 +100,7 @@ export function App() {
 					(session): View => ({
 						id: session.id,
 						label: session.playerName,
-						sublabel: server.name,
+						sublabel: session.gameName,
 						icon: "mingcute:game-2-fill",
 						content: (
 							<p>
@@ -183,7 +183,15 @@ export function App() {
 							]}
 						>
 							{view.sessionViews.map((view) => (
-								<NavButton key={view.id} {...navItemProps(view)} />
+								<NavButton
+									key={view.id}
+									{...navItemProps(view)}
+									onClose={() => {
+										setSessions((sessions) =>
+											sessions.filter((s) => s.id !== view.id),
+										)
+									}}
+								/>
 							))}
 						</NavCollapse>
 					))}
@@ -204,27 +212,58 @@ interface NavButtonProps extends React.ComponentProps<"button"> {
 	label: ReactNode
 	sublabel?: ReactNode
 	isCurrent: boolean
+	onClose?: () => void
 }
 
-function NavButton(props: NavButtonProps) {
+function NavButton({
+	icon,
+	label,
+	sublabel,
+	isCurrent,
+	className,
+	onClick,
+	onClose,
+	...props
+}: NavButtonProps) {
 	return (
-		<button
-			type="button"
-			data-current={props.isCurrent || undefined}
-			{...props}
-			className={twMerge(
-				"flex min-h-10 w-full items-center justify-start gap-2 rounded px-3 py-2 opacity-75 -outline-offset-2 transition duration-150 hover:bg-gray-800 hover:opacity-100 data-current:bg-gray-800 data-current:opacity-100",
-				props.className,
-			)}
+		<div
+			className="flex w-full items-center rounded opacity-75 transition hover:bg-gray-800 hover:opacity-100 data-current:bg-gray-800 data-current:opacity-100"
+			data-current={isCurrent || undefined}
 		>
-			<Icon icon={props.icon} className="-mx-0.5 size-5 shrink-0" />
-			<div className="flex min-w-0 flex-1 flex-col text-start">
-				<span className="truncate text-base/tight">{props.label}</span>
-				<span className="truncate text-gray-300/90 text-xs/tight">
-					{props.sublabel}
-				</span>
-			</div>
-		</button>
+			<button
+				type="button"
+				onClick={onClick}
+				className={twMerge(
+					"flex min-h-10 flex-1 items-center justify-start gap-2 rounded px-3 py-2 -outline-offset-2 transition",
+					className,
+				)}
+				{...props}
+			>
+				<Icon icon={icon} className="-mx-0.5 size-5 shrink-0" />
+				<div className="flex min-w-0 flex-1 flex-col text-start">
+					<span className="truncate text-base/tight">{label}</span>
+					<span className="truncate text-gray-300/90 text-xs/tight">
+						{sublabel}
+					</span>
+				</div>
+			</button>
+
+			<button
+				type="button"
+				data-visible={isCurrent && onClose ? true : undefined}
+				className="flex size-8 items-center justify-center rounded opacity-0 -outline-offset-2 transition data-visible:opacity-100"
+				onClick={(event) => {
+					if (isCurrent) {
+						onClose?.()
+					} else {
+						onClick?.(event)
+					}
+				}}
+			>
+				<Icon icon="mingcute:close-fill" className="-mx-0.5 size-5 shrink-0" />
+				<span className="sr-only">Close</span>
+			</button>
+		</div>
 	)
 }
 
