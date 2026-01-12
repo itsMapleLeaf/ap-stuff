037eebb0f23ad4f74144a5979b1caf32e0503954
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2026-01-11 18:39:12 -0600

    range_inclusive: implement start/end

--- manual_worlds/kirby_air_riders/lib/helpers.py
+++ manual_worlds/kirby_air_riders/lib/helpers.py
@@ -1,3 +1,12 @@
-def range_inclusive(len: int):
-    for i in range(len):
+from typing import Union, Unpack
+
+
+def range_inclusive(start_or_len: int, end_arg: int | None = None):
+    match (start_or_len, end_arg):
+        case (start, int(end)):
+            pass
+        case (len, None):
+            start, end = 0, len
+
+    for i in range(start, end):
         yield i + 1
