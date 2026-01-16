5bcdebcb2337d1b9adb6d59667f026c7611478d8
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2026-01-16 16:03:44 -0600

    pointless organizing

--- saika/__main__.py
+++ saika/__main__.py
@@ -9,88 +9,35 @@ from .lib.http import wait_until_reachable
 from .lib.subprocess import ensure_killed
 
 
-def __main():
-    # tracker_thread = threading.Thread(
-    #     target=lambda: asyncio.run(__run_tracker()),
-    #     daemon=True,
-    # )
-    # tracker_thread.start()
-    asyncio.run(__run_webview())
+async def main():
+    server_host = "localhost"
+    server_port = 5173
+    server_url = f"http://{server_host}:{server_port}"
 
-
-async def __run_webview():
-    async with await __start_dev_server():
-        view_url = "http://localhost:5173/"
-        await wait_until_reachable(view_url, timeout_seconds=10)
-
-        class Api:
-            def log(self, *values):
-                print(*values)
-
-        webview.create_window(title="Saika", url=view_url, js_api=Api())
-        webview.start(ssl=True, debug=True)
-
-
-async def __start_dev_server():
-    config = {
+    process_config = {
         "cwd": Path(__file__).parent / "web",
         "stdin": DEVNULL,
     }
 
     if sys.platform == "win32":
-        config["creationflags"] = subprocess.CREATE_NEW_PROCESS_GROUP
+        process_config["creationflags"] = subprocess.CREATE_NEW_PROCESS_GROUP
 
     server = await asyncio.subprocess.create_subprocess_exec(
-        "bun",
-        "dev",
-        **config,
+        *("bun", "dev"),
+        *("--host", server_host),
+        *("--port", str(server_port)),
+        **process_config,
     )
 
-    return ensure_killed(server)
+    async with ensure_killed(server):
+        await wait_until_reachable(server_url, timeout_seconds=10)
+
+        class Api:
+            def log(self, *values):
+                print(*values)
+
+        webview.create_window(title="Saika", url=server_url, js_api=Api())
+        webview.start(ssl=True, debug=True)
 
 
-async def __run_tracker():
-    print("running tracker")
-
-    from worlds.tracker.TrackerClient import TrackerGameContext, server_loop
-
-    class SaikaContext(TrackerGameContext):
-        updated = asyncio.Future()
-
-        def __init__(self):
-            super().__init__("localhost", None)
-
-            self.set_callback(self.on_update)
-            self.set_events_callback(self.on_update_events)
-
-            self.auth = "MapleVoltex"
-            self.server_task = asyncio.create_task(
-                server_loop(self), name="server loop"
-            )
-
-            print("running generator")
-            self.run_generator()
-
-        def on_update(self, locations: list[str]):
-            print(f"{locations=}")
-            if not self.updated.done():
-                self.updated.set_result(None)
-            return True
-
-        def on_update_events(self, events: list[str]):
-            print(f"{events=}")
-            return True
-
-    ctx = SaikaContext()
-
-    try:
-        assert ctx.server_task
-        await asyncio.wait(
-            [ctx.server_task, ctx.updated],
-            return_when=asyncio.FIRST_COMPLETED,
-        )
-    finally:
-        await ctx.shutdown()
-
-
-__main()
+asyncio.run(main())
